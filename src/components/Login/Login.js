c8fff77f4ee20ed3570cd1c4fc120657bd5c14a3
Michael B <michaelbahreini@gmail.com>
2021-10-15 04:01:09 -0500

    added events -onchange and handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 
 import classes from "./Login.module.css";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [pw, setPw] = useState("");
+
+  const onUsernameHandler = function (e) {
+    setUsername(e.target.value);
+  };
+
+  const onPasswordHandler = function (e) {
+    setPw(e.target.value);
+  };
+
   const onSubmitHandler = function (e) {
     e.preventDefault();
   };
@@ -13,11 +24,21 @@ function Login() {
       <form onSubmit={onSubmitHandler}>
         <div className={classes.fields}>
           <label htmlFor="username">Username: </label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={onUsernameHandler}
+          />
         </div>
         <div className={classes.fields}>
           <label htmlFor="pw">Password: </label>
-          <input type="password" id="pw" />
+          <input
+            type="password"
+            id="pw"
+            value={pw}
+            onChange={onPasswordHandler}
+          />
         </div>
         <div className={classes.btn}>
           <button type="submit">Login</button>
